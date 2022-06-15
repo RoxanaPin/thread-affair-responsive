8d9972c9d63a6c851007e8a291e543e3237af1aa
RoxanaPin <100729346+RoxanaPin@users.noreply.github.com>
2022-06-15 22:18:47 +0300

    thread aff responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,17 +2,22 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div  >
+    <div>
       <Head>
-        <title>Thread affair</title>
+        <title>Thead Affair</title>
       </Head>
 
-      <header className="header">Hello world!
-      <img src="/images/logo.jpg" alt=""></img>
-      <i className="fa-solid fa-phone"></i>
+      <header className="header mt-5 text-warning">
+        Hello
+        <img src="/images/logo.jpg" alt=""></img>
+        <i className="fa-solid fa-phone"></i>
       </header>
 
-      <main className="content"></main>
+      <main className="content">
+        <div className="badge">20off</div>
+        <div className="badge badge-secondary">new entry</div>
+        <div className="badge badge-oversized">%</div>
+      </main>
 
       <footer className="footer"></footer>
     </div>
