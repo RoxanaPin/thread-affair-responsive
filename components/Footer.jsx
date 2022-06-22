dd2363e3bff8433208b7988986ea6475d7fc44b8
RoxanaPin <100729346+RoxanaPin@users.noreply.github.com>
2022-06-22 20:38:12 +0300

    l4

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+export default function Footer() {
+  return (
+    <footer className="footer ">
+      <section className="footer-primary">
+        <div className="footer-primary-inner container">x</div>
+      </section>
+
+      <section className="footer-secondary text-center mt-4">
+        <p>©2019 THREAD AFFAIR. All Rights Reserved.</p>
+
+        <p>
+          Designed by
+          <img
+            title=""
+            data-test=""
+            className="ms-2"
+            src="/images/pixellab_logo.jpg"
+            alt="Pixellab"
+          ></img>
+        </p>
+      </section>
+    </footer>
+  );
+}
